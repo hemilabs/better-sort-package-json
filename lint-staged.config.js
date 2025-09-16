dccbf3000847f17f095b82908f3df55fc7e6cad7
Gabriel Montes <gabriel@hemi.xyz>
2025-09-16 15:04:27 -0400

    Auto format GHA workflows

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,12 @@
 const formatFiles = "prettier --ignore-unknown --write";
 const lintFiles = "eslint --cache --fix --quiet";
+const sortGithubActions = "better-sort-github-actions";
 const sortPackageJsonFiles = "node bin/cli.js";
 
 const config = {
   "!(*.{js,md}|package.json)": [formatFiles],
   "*.{js,md}": [lintFiles, formatFiles],
+  ".github/workflows/*.yml": [sortGithubActions, formatFiles],
   "package.json": [sortPackageJsonFiles, formatFiles],
 };
 
